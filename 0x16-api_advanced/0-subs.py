c8a58933b9b7e92f5c0695d0a67c0fddc20b2b98
toukali <141361989+toukali@users.noreply.github.com>
2024-05-31 01:24:45 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
                             .format(subreddit),
-                            headers={"User-Agent": 'Mozilla/5.0'},
+                            headers={"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'},
                                      allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
